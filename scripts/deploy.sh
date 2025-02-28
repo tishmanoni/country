e593c440fc8ae11c597efb1a2d3d515f675543a4
tishmanoni <66375712+tishmanoni@users.noreply.github.com>
2025-02-28 00:28:04 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-cd /var/www/html
-git pull origin development
-sudo systemctl restart httpd
+sudo apt update -y
+sudo apt install -y apache2 php php-mysql mysql-client
+sudo systemctl enable apache2
+sudo systemctl start apache2
 
+# Deploy website files
+sudo rm -rf /var/www/html/*
+sudo git clone https://github.com/YOUR_GITHUB_USERNAME/YOUR_REPOSITORY.git /var/www/html/
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
+
+# Restart Apache
+sudo systemctl restart apache2
